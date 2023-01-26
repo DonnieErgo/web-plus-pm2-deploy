aae75d014417e95c254f58768e3c0c99fddb3f28
Mikhail Nenashev <nord-vizjerei@yandex.ru>
2023-01-26 05:14:12 +0200

    config fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy' : 'cd ~/web-plus-pm2-deploy/frontend/source/frontend/ && npm i && npm run build',
+      'post-deploy' : 'cd frontend && npm i&& npm run build',
     }
   }
 };
